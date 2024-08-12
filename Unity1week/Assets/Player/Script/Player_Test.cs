c99850f73e26eae01b38bca0b184acb91f1d3155
mocha_show <fuka.sho0224@gmail.com>
2024-08-12 15:25:32 +0900

    inputsystemの追加

--- Unity1week/Assets/Player/Script/Player_Test.cs
+++ Unity1week/Assets/Player/Script/Player_Test.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Test : MonoBehaviour
 {
-
+    [SerializeField] private PlayerInput MoveAction;
+    private InputAction moveAction;
+    private Vector2 InputMove = Vector2.zero;
     public float Speed=3;
     public float JampForce = 200;
 
     Rigidbody2D rb;
 
     public enum MoveMode 
-    { 
-    
-        Floor,
-        Ceiling
+    {
 
+        Ceiling = 1,
+        Floor = -1
+     
     }
 
     public MoveMode moveMode = MoveMode.Floor;
@@ -24,57 +27,74 @@ public class Player_Test : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        MoveAction.actions["Move"].performed += OnMove;
+        MoveAction.actions["Move"].canceled += OnMove;
+        MoveAction.actions["Jump"].started += Jump;
+        MoveAction.actions["GravityChange"].started += GravityChange;
+        moveAction = MoveAction.actions["Move"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputAction.CallbackContext context)
     {
-        
+        InputMove = context.ReadValue<Vector2>();
+      
+    }
 
-        if (moveMode == MoveMode.Floor)
-        {
-            transform.localScale = new Vector3(transform.localScale.x,2, transform.localScale.z);
-
-            rb.gravityScale = 1;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.AddForce(transform.up * JampForce);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                moveMode = MoveMode.Ceiling;
-            }
-
-        }
-        else 
-        {
-            transform.localScale = new Vector3(transform.localScale.x, -2, transform.localScale.z);
-            rb.gravityScale = -1;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.AddForce(transform.up * -JampForce);
-            }
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                moveMode = MoveMode.Floor;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.A)) 
-        {
-            rb.velocity = new Vector2(-Speed, rb.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.D))
+    private void Move()
+    {
+        if (InputMove.x >= 0.1f)
         {
             rb.velocity = new Vector2(Speed, rb.velocity.y);
         }
+        if (InputMove.x <= -0.1f)
+        {
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+        }
+    }
+    private void Stop()
+    {
+        rb.velocity = new Vector2(0,rb.velocity.y);
+    }
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (moveMode == MoveMode.Floor)
+            rb.AddForce(transform.up * -JampForce);
+
         else
-            rb.velocity = Vector2.zero;
+            rb.AddForce(transform.up * JampForce);
+
+       
+    }
+
+    private void GravityChange(InputAction.CallbackContext context)
+    {
+        if (moveMode == MoveMode.Floor)
+            moveMode = MoveMode.Ceiling;
+        else
+            moveMode = MoveMode.Floor;
+
+        rb.gravityScale = (float)moveMode;
 
     }
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
+
+        if (moveMode == MoveMode.Floor)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, -2, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, 2, transform.localScale.z);
+          
+        }
+
+        if (InputMove != Vector2.zero)
+            Move();
         
+        else
+            Stop();
     }
+   
 }
