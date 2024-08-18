245266de1cf79311a87240858d0dd8c28b9ba897
mocha_show <fuka.sho0224@gmail.com>
2024-08-18 18:43:49 +0900

    ステージ４の修正

--- Unity1week/Assets/Scripts/TitleController.cs
+++ Unity1week/Assets/Scripts/TitleController.cs
@@ -18,7 +18,7 @@ public class TitleController : MonoBehaviour
 
     private void Start()
     {
-        PlayerPrefs.DeleteAll();
+
         soundManager.SetGameBGM();
     }
     private void Update()
